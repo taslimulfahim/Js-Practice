12936747fa320bf433b45b309917aadaedbbf166
Taslimul hasan Fahim <121567834+taslimulfahim@users.noreply.github.com>
2023-12-17 04:40:31 +0000

    Variables Pratice done

---
+++ 01_basics/01_varieables.js
@@ -0,0 +1,14 @@
+const accountId= 135455
+let accountName = 'Fahim'
+var accountPass = '12345'
+
+accountId= 333333;
+accountName= 'Sazid'
+accountPass= '000000'
+
+console.table([accountId, accountName, accountPass])
+
+/*
+ Prefer not to use var
+ because of issue in block scope and functional scope
+*/
