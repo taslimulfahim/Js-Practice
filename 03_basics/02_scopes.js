62442f27a0571caf3f4780aa19a3f811fdc9ef18
taslimulhasanfahim <taslimulhasanfahim@gmail.com>
2023-12-28 22:33:26 +0600

    understanding_Scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,4 +6,41 @@ if (true) {
 
 // console.log(a)
 // console.log(b)
-console.log(c);
+// console.log(c);
+
+function one() {
+  const username = "Fahim";
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  //   two();
+}
+
+// You can access data from your head scope which is considered as a global scope for it but if you want to access data from your child scope which is considered as a block scope you can't access data from it which is explain in the above code.
+
+// one();
+
+if (true) {
+  const username = "hitesh";
+  if (username === "hitesh") {
+    const website = " youtube";
+    // console.log(username + website);
+  }
+  //   console.log(website);
+}
+
+// +++++++++++++++++++++ Interesting ++++++++++++++++++
+addOne(5) // If you call function on above it then it will execute without giving error.
+
+function addOne(num) {
+  return num + 1;
+}
+
+
+addTwo(5) // But if you declare a function in variable and call it above function now will show you an error that's called hoisting type of something...
+
+const addTwo = function (num) {
+    return num +2
+}
+
