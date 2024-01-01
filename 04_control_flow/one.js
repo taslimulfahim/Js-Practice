691ae3e3f698c803464f2797818957f9f178b2e2
taslimulhasanfahim <taslimulhasanfahim@gmail.com>
2024-01-01 07:25:47 +0600

    Understanding Js Control Flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,41 @@
+// if
+const isUserLoggedIn = true;
+const temperature = 32;
+
+// if (temperature < 50) {
+//   console.log("Temperature is less than 50");
+// } else {
+//   console.log("Temperature is greater than 50");
+// }
+
+// <, >, <=, >=, ==, !=,
+// ===
+
+// const score = 200;
+// if (score > 100) {
+//   const power = "fly";
+//   console.log(`Power is ${power}`);
+// }
+
+// console.log(`Power is ${power}`);
+
+//* Its called implicit
+// const balance = 1000;
+// if (balance > 500) console.log('test')
+
+// if (balance < 500) {
+//   console.log("greater than 500");
+// } else if (balance < 750) {
+//   console.log("greater than 750");
+// } else if (balance < 900) {
+//   console.log("greater than 900");
+// } else {
+//   console.log("less than 1200");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+
+if (userLoggedIn && debitCard) {
+  console.log("Allow to buy course");
+}
