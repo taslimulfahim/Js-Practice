ed82e7e7e4a9e9021befd2c175eb78de8a8117e8
taslimulhasanfahim <taslimulhasanfahim@gmail.com>
2024-01-01 15:21:52 +0600

    While and do While loop understood

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+//* while
+// let index = 1;
+// while (index <= 10) {
+//   console.log(index);
+//   index = index + 2;
+// }
+
+// let myArr = ["Ali", "Omor", "Hamza"];
+// let arr = 0;
+
+// while (arr < myArr.length) {
+//   console.log(`${myArr[arr]}`);
+//   arr = arr + 1;
+// }
+
+//* do while
+let score = 11;
+
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
