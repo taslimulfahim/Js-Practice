9c47d389bcf7b4f4f0df7fb08a550bfcaeffc6bd
taslimulhasanfahim <taslimulhasanfahim@gmail.com>
2024-01-02 07:25:13 +0600

    Maps and For of Understood

---
+++ 05_iterations/four.js
@@ -0,0 +1,10 @@
+const myObj = {
+  js: "JavasCript",
+  cpp: "C++",
+  rb: "ruby",
+  swift: "swift by apple",
+};
+
+for (const key in myObj) {
+    console.log(myObj[key])
+}
---
+++ 05_iterations/three.js
@@ -0,0 +1,32 @@
+//* for of
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const num of arr) {
+  // console.log(num);
+}
+
+const greetings = "Hello World";
+for (const greet of greetings) {
+  if (greet === " ") {
+    continue;
+  }
+  // console.log(greet);
+}
+
+//* Maps
+
+const mapTest = new Map();
+
+mapTest.set("BD", "Bangladesh");
+mapTest.set("PAK", "Pakistan");
+mapTest.set("AFG", "Afghanistan");
+
+// console.log(mapTest)
+
+for (const [i, id] of mapTest) {
+  // console.log(i, id);
+}
+
+
+
