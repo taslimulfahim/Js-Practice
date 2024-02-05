946dfc1c1dd73a1394f4b0d2441f5144f327621a
taslimulfahim <taslimulhasanfahim@gmail.com>
2024-02-05 21:15:40 +0600

    d

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -16,8 +16,9 @@ const anotherArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
 // console.log(Array.from('Fahim'))
 // console.log(Array.from({name: 'Fahim'})) //* Interesting
 
-let sc1 = 100
-let sc2 = 200
-let sc3 = 300
+let sc1 = 100;
+let sc2 = 200;
+let sc3 = 300;
 
-console.log(Array.of(sc1, sc2, sc3))
+console.log(Array.of(sc1, sc2, sc3));
+
